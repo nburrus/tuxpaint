2a430c0dc04f7c2dbc39a64c6dfc5903cf04241b
Pere Pujal i Carabantes <perepujal@gmail.com>
2020-05-05 00:24:42 +0200

    Undeffing FORKED_FONTS seems to improve things on Android. The system onscreen keyboard hid the progress bar, don't show it until fonts are already in place.

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -3295,13 +3295,6 @@ static void mainloop(void)
                                   SDL_BlitSurface(kbd->surface, &kbd->rect, screen, &kbd_rect);
                                   update_screen_rect(&kbd_rect);
                                 }
-
-                              if (onscreen_keyboard && !kbd)
-                                {
-                                  r_tir.y = (float)event.button.y / render_scale;
-                                  SDL_SetTextInputRect(&r_tir);
-                                  SDL_StartTextInput();
-                                }
                               if (!font_thread_done)
                                 {
                                   draw_colors(COLORSEL_DISABLE);
@@ -3328,6 +3321,12 @@ static void mainloop(void)
 #endif
                                   set_label_fonts();
                                   do_setcursor(cursor_arrow);
+                                }
+                              if (onscreen_keyboard && !kbd)
+                                {
+                                  r_tir.y = (float)event.button.y / render_scale;
+                                  SDL_SetTextInputRect(&r_tir);
+                                  SDL_StartTextInput();
                                 }
                               draw_tux_text(tool_tux[cur_tool], tool_tips[cur_tool], 1);
 
@@ -24450,7 +24449,7 @@ static void setup(void)
 #ifdef FORKED_FONTS
   reliable_write(font_socket_fd, &no_system_fonts, sizeof no_system_fonts);
 #else
-  font_thread = SDL_CreateThread(load_user_fonts_stub, NULL);
+  font_thread = SDL_CreateThread(load_user_fonts_stub, "font_thread", NULL);
 #endif
 
   /* continuing on with the rest of the cursors... */
