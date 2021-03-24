8db052c9d3968eba338552848a186251f21ee4c1
Mark Kim <markuskimius@gmail.com>
2021-03-23 22:26:49 -0400

    fix attempt to free non-freeable memory

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -23961,7 +23961,7 @@ static void setup_config(char *argv[])
 #ifdef WIN32
       picturesdir = GetUserImageDir();
 #elif __APPLE__
-      picturesdir = apple_picturesPath();
+      picturesdir = strdup(apple_picturesPath());
 #else
       picturesdir = get_xdg_user_dir("PICTURES", "Pictures");
 #endif
