2cc4217a0c78bb7dfcc3074d457c6af36a743a61
Mark Kim <markuskimius@gmail.com>
2022-02-12 22:13:26 -0500

    macOS check for locale fonts in Tux Paint system preferences location

--- src/fonts.c
+++ src/fonts.c
@@ -960,6 +960,8 @@ static void loadfonts_locale_filter(SDL_Surface * screen, const char *const dir,
   char buf[TP_FTW_PATHSIZE];
   unsigned dirlen = strlen(dir);
 
+  DEBUG_PRINTF("Loading fonts from [%s]\n", dir);
+
   memcpy(buf, dir, dirlen);
   tp_ftw(screen, buf, dirlen, 1, loadfont_callback, locale);
 }
@@ -1032,6 +1034,15 @@ static void loadfonts(SDL_Surface * screen, const char *const dir)
   homedirdir = get_fname("data/fonts", DIR_DATA);
   loadfonts(screen, homedirdir);
   free(homedirdir);
+#endif
+
+#ifdef __APPLE__
+  homedirdir = malloc(snprintf(NULL, 0, "%s/fonts", apple_globalPreferencesPath()) + 1);
+  if(homedirdir) {
+    sprintf(homedirdir, "%s/fonts", apple_globalPreferencesPath());
+    loadfonts(screen, homedirdir);
+    free(homedirdir);
+  }
 #endif
 
 #ifdef DEBUG
