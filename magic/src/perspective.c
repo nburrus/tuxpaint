ff470263a0bcb851975ca1bde5e8cbeabaf6271d
Bill Kendrick <nbs@sonic.net>
2022-06-02 21:17:23 -0700

    Different description for Tile Zoom than plain Zoom
    Oops, had committed to sdl2.0 only :eyeroll:

--- magic/src/perspective.c
+++ magic/src/perspective.c
@@ -32,7 +32,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   (See COPYING.txt)
 
-  Last updated: May 7, 2022
+  Last updated: June 2, 2022
   $Id$
 */
 
@@ -170,7 +170,7 @@ const char *perspective_descs[perspective_NUM_TOOLS] = {
 
   gettext_noop("Click to turn your picture into 2-by-2 panels."),
 
-  gettext_noop("Click and drag up to zoom in or drag down to zoom out the picture."),
+  gettext_noop("Click and drag up to zoom in the picture. Drag down to zoom out and tile the picture."),
 
   gettext_noop("Click and drag up to zoom in or drag down to zoom out the picture."),
 
@@ -190,7 +190,7 @@ int perspective_init(magic_api * api)
 
   for (i = 0; i <= perspective_NUM_TOOLS; i++)
     {
-      snprintf(fname, sizeof(fname), "%s/sounds/magic/%s", api->data_directory, perspective_snd_filenames[i]);
+      snprintf(fname, sizeof(fname), "%ssounds/magic/%s", api->data_directory, perspective_snd_filenames[i]);
       perspective_snd_effect[i] = Mix_LoadWAV(fname);
     }
   return (1);
