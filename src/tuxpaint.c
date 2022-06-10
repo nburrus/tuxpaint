f8387042820c95521f5e8b3b4d835a4249c2b3f6
dolphin6k <dolphin6k@wmail.plala.or.jp>
2022-06-10 20:46:48 +0900

    Fixed jugged upscaling in full screen mode

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -27555,6 +27555,7 @@ static void setup(void)
 #endif
 
       renderer = SDL_CreateRenderer(window_screen, -1, 0);
+      SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
 
       if (native_screensize)
         {
@@ -27570,7 +27571,6 @@ static void setup(void)
               window_scale_w = 501.f / ww;
               window_scale_h = 481.f / hh;
 
-	      SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
               if (window_scale_w > window_scale_h)
                 {
                   /* Keep things squared */
