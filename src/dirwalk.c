935ffb4839641f137ac9566526910a370503770e
dolphin6k <dolphin6k@wmail.plala.or.jp>
2022-12-18 00:07:11 +0900

    Forgot to multiply the score by 10

--- src/dirwalk.c
+++ src/dirwalk.c
@@ -260,7 +260,7 @@ void loadfont_callback(SDL_Surface * screen, SDL_Texture * texture,
             font, /* distinct line-like characters (e.g., 'l' (lowercase elle) vs. '1' (one) vs. 'I' (capital aye)) */ gettext("1Il|")
           );
 
-          user_font_styles[num_font_styles]->score +=
+          user_font_styles[num_font_styles]->score += 10 *
             charset_works(
             font, /* put locale specific characters to the translation for this string to score up the fonts supporting those characters. */ gettext("ABC...xyz")
           );
