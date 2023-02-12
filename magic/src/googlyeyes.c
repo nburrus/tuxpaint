4d932f800be2a22d3e747e24c18919588b93c05d
Bill Kendrick <nbs@sonic.net>
2023-02-12 13:17:49 -0800

    Add ATTRIBUTE_UNUSED back to MAGIC_set_color() funcs
    Avoid compile time warnings

--- magic/src/googlyeyes.c
+++ magic/src/googlyeyes.c
@@ -3,7 +3,7 @@
    Draws a googly eye at the click position, and looks
    towards where you drag+release.
 
-  Last updated: February 11, 2023
+  Last updated: February 12, 2023
 */
 
 #include <stdio.h>
@@ -295,8 +295,8 @@ googlyeyes_release(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED,
 }
 
 
-void googlyeyes_set_color(magic_api * api, int which, SDL_Surface * canvas,
-                          SDL_Surface * last, Uint8 r, Uint8 g, Uint8 b, SDL_Rect * update_rect)
+void googlyeyes_set_color(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, SDL_Surface * canvas ATTRIBUTE_UNUSED,
+                          SDL_Surface * last ATTRIBUTE_UNUSED, Uint8 r ATTRIBUTE_UNUSED, Uint8 g ATTRIBUTE_UNUSED, Uint8 b ATTRIBUTE_UNUSED, SDL_Rect * update_rect ATTRIBUTE_UNUSED)
 {
 }
 
