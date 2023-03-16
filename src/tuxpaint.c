b2ac96532876afb6beeba60be3b05c932d7c285b
Bill Kendrick <nbs@sonic.net>
2023-03-15 21:24:08 -0700

    Bump date in tuxpaint.c & CHANGES.txt

--- src/tuxpaint.c
+++ src/tuxpaint.c
@@ -22,7 +22,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   (See COPYING.txt)
 
-  June 14, 2002 - March 7, 2023
+  June 14, 2002 - March 15, 2023
 */
 
 #include "platform.h"
