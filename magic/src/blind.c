9bf7cdd503f3cedaec5e69706b0de5df5465fd30
Bill Kendrick <nbs@sonic.net>
2023-04-18 22:33:02 -0700

    Blind: Update for new plugin API

--- magic/src/blind.c
+++ magic/src/blind.c
@@ -24,7 +24,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   (See COPYING.txt)
 
-  Last updated: February 12, 2023
+  Last updated: April 18, 2023
 */
 
 #include "tp_magic_api.h"
@@ -57,7 +57,7 @@ Mix_Chunk *blind_snd;
 Uint32 blind_api_version(void);
 void blind_set_color(magic_api * api, int which, SDL_Surface * canvas,
                      SDL_Surface * last, Uint8 r, Uint8 g, Uint8 b, SDL_Rect * update_rect);
-int blind_init(magic_api * api);
+int blind_init(magic_api * api, Uint32 disabled_features);
 int blind_get_tool_count(magic_api * api);
 SDL_Surface *blind_get_icon(magic_api * api, int which);
 char *blind_get_name(magic_api * api, int which);
@@ -80,6 +80,9 @@ void blind_switchin(magic_api * api, int which, int mode,
 void blind_switchout(magic_api * api, int which, int mode,
                      SDL_Surface * canvas);
 int blind_modes(magic_api * api, int which);
+Uint8 blind_accepted_sizes(magic_api * api, int which, int mode);
+Uint8 blind_default_size(magic_api * api, int which, int mode);
+void blind_set_size(magic_api * api, int which, int mode, SDL_Surface * canvas, SDL_Surface * last, Uint8 size, SDL_Rect * update_rect);
 
 //                              Housekeeping functions
 
@@ -97,7 +100,7 @@ void blind_set_color(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSE
   blind_b = b;
 }
 
-int blind_init(magic_api * api)
+int blind_init(magic_api * api, Uint32 disabled_features ATTRIBUTE_UNUSED)
 {
   char fname[1024];
 
@@ -348,4 +351,18 @@ void blind_switchout(magic_api * api ATTRIBUTE_UNUSED,
 int blind_modes(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED)
 {
   return (MODE_FULLSCREEN | MODE_PAINT);
+}
+
+Uint8 blind_accepted_sizes(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, int mode ATTRIBUTE_UNUSED)
+{
+  return 0;
+}
+
+Uint8 blind_default_size(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, int mode ATTRIBUTE_UNUSED)
+{
+  return 0;
+}
+
+void blind_set_size(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, int mode ATTRIBUTE_UNUSED, SDL_Surface * canvas ATTRIBUTE_UNUSED, SDL_Surface * last ATTRIBUTE_UNUSED, Uint8 size ATTRIBUTE_UNUSED, SDL_Rect * update_rect ATTRIBUTE_UNUSED)
+{
 }
