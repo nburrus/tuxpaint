b9372e8a3b1218b6073abe660df2d7f8c83c5a1c
Bill Kendrick <nbs@sonic.net>
2023-04-19 22:41:48 -0700

    Sharpen, Edges, Silhouette: Support sizes

--- magic/src/sharpen.c
+++ magic/src/sharpen.c
@@ -25,7 +25,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   (See COPYING.txt)
 
-  Last updated: February 12, 2023
+  Last updated: April 19, 2023
 */
 
 #include <stdio.h>
@@ -53,7 +53,7 @@ enum
 
 static const int THRESHOLD = 50;
 
-static const int sharpen_RADIUS = 16;
+static int sharpen_RADIUS = 16;
 
 static const double SHARPEN = 0.5;
 
@@ -90,7 +90,7 @@ const char *sharpen_descs[sharpen_NUM_TOOLS][2] = {
 };
 
 Uint32 sharpen_api_version(void);
-int sharpen_init(magic_api * api);
+int sharpen_init(magic_api * api, Uint32 disabled_features ATTRIBUTE_UNUSED);
 int sharpen_get_tool_count(magic_api * api);
 SDL_Surface *sharpen_get_icon(magic_api * api, int which);
 char *sharpen_get_name(magic_api * api, int which);
@@ -122,6 +122,11 @@ void sharpen_switchin(magic_api * api, int which, int mode,
 void sharpen_switchout(magic_api * api, int which, int mode,
                        SDL_Surface * canvas);
 int sharpen_modes(magic_api * api, int which);
+Uint8 sharpen_accepted_sizes(magic_api * api, int which, int mode);
+Uint8 sharpen_default_size(magic_api * api, int which, int mode);
+void sharpen_set_size(magic_api * api, int which, int mode, SDL_Surface * canvas, SDL_Surface * last, Uint8 size, SDL_Rect * update_rect);
+
+
 
 Uint32 sharpen_api_version(void)
 {
@@ -130,7 +135,7 @@ Uint32 sharpen_api_version(void)
 
 
 // No setup required:
-int sharpen_init(magic_api * api)
+int sharpen_init(magic_api * api, Uint32 disabled_features ATTRIBUTE_UNUSED)
 {
 
   int i;
@@ -399,4 +404,24 @@ int sharpen_modes(magic_api * api ATTRIBUTE_UNUSED,
                   int which ATTRIBUTE_UNUSED)
 {
   return (MODE_FULLSCREEN | MODE_PAINT);
+}
+
+
+
+Uint8 sharpen_accepted_sizes(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, int mode ATTRIBUTE_UNUSED)
+{
+  if (mode == MODE_PAINT)
+    return 8;
+  else
+    return 0;
+}
+
+Uint8 sharpen_default_size(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, int mode ATTRIBUTE_UNUSED)
+{
+  return 4;
+}
+
+void sharpen_set_size(magic_api * api ATTRIBUTE_UNUSED, int which ATTRIBUTE_UNUSED, int mode ATTRIBUTE_UNUSED, SDL_Surface * canvas ATTRIBUTE_UNUSED, SDL_Surface * last ATTRIBUTE_UNUSED, Uint8 size, SDL_Rect * update_rect ATTRIBUTE_UNUSED)
+{
+  sharpen_RADIUS = size * 4;
 }
