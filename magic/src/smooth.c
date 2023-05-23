da02bf19bffce18e461fc4a1d04464108fb8b911
Bill Kendrick <nbs@sonic.net>
2023-05-22 23:43:28 -0700

    Loops magic tool sound
    https://freesound.org/people/Jagadamba/sounds/254397/
    CC-BY 4.0 https://freesound.org/people/Jagadamba/

--- magic/src/smooth.c
+++ magic/src/smooth.c
@@ -49,7 +49,7 @@ enum {
 char * smooth_snd_fnames[NUM_TOOLS] = {
   "rainbow.ogg", // FIXME
   "rainbow.ogg", // FIXME
-  "rainbow.ogg", // FIXME
+  "loops.ogg",
 };
 
 char * smooth_names[NUM_TOOLS] = {
