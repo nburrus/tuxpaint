dadcc9ba2dccbfe427d691a6360063a0be61c4a0
Bill Kendrick <nbs@sonic.net>
2023-07-17 14:34:12 -0700

    TODO for dealing with "forbidden" wordwrap characters

--- docs/w3m.sh
+++ docs/w3m.sh
@@ -3,5 +3,11 @@
 infile=${@: -1}
 args=${@:1:${#}-1}
 
-sed -e "s/<\\/dd>/<br\\/><br\\/><\\/dd>/g" $infile | w3m $args
+# FIXME: https://en.wikipedia.org/wiki/Line_breaking_rules_in_East_Asian_languages
+# | sed -e "s/\(.[、。）]\)/<nobr>\\1<\\/nobr>/g" \
+# This needs expanding & to not mess with <img> `alt` tag attributes or anything inside HTML tags (e.g., " quotes)!
+# -bjk 2023.07.17
+
+sed -e "s/<\\/dd>/<br\\/><br\\/><\\/dd>/g" $infile \
+  | w3m $args
 
