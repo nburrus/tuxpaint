e9db2402b1649e058167df3ba6feec35f9c578f1
Bill Kendrick <nbs@sonic.net>
2024-09-28 22:50:09 -0700

    "Crescent" sound effect

--- magic/src/crescent.c
+++ magic/src/crescent.c
@@ -82,7 +82,7 @@ int crescent_init(magic_api * api, Uint8 disabled_features ATTRIBUTE_UNUSED, Uin
 {
   char fname[1024];
 
-  snprintf(fname, sizeof(fname), "%ssounds/magic/xor.ogg", api->data_directory); // FIXME
+  snprintf(fname, sizeof(fname), "%ssounds/magic/crescent.ogg", api->data_directory);
   crescent_snd = Mix_LoadWAV(fname);
 
   return (1);
